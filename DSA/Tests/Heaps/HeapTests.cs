81f3ba17dcacddc03515c94c38dbedac673f82f1
netotz <neto.otz@hotmail.com>
2023-01-17 15:18:48 -0600

    test(heaps): assert new sizes

--- DSA/Tests/Heaps/HeapTests.cs
+++ DSA/Tests/Heaps/HeapTests.cs
@@ -27,19 +27,21 @@ public class HeapTests
     }
 
     [Theory]
-    [InlineData(new[] { 0, 1 }, 1, 0)]
-    [InlineData(new[] { 0, 1, 2 }, 2, 1)]
-    [InlineData(new[] { 0, 3, 1, 2 }, 3, 2)]
-    [InlineData(new[] { 0, 3, 1, 2, 4 }, 4, 3)]
-    public void Pop_ReturnsRootAndHeapifies(int[] values, int popped, int newRoot)
+    [InlineData(new[] { 0, 1 }, 1, 0, 1)]
+    [InlineData(new[] { 0, 1, 2 }, 2, 1, 2)]
+    [InlineData(new[] { 0, 3, 1, 2 }, 3, 2, 3)]
+    [InlineData(new[] { 0, 3, 1, 2, 4 }, 4, 3, 4)]
+    public void Pop_ReturnsRootAndHeapifies(int[] values, int popped, int newRoot, int newSize)
     {
         var maxHeap = new MaxHeap<int>(values);
 
         var actualPopped = maxHeap.Pop();
         var actualPeaked = maxHeap.Peek();
+        var actualSize = maxHeap.Size;
 
         Assert.Equal(popped, actualPopped);
         Assert.Equal(newRoot, actualPeaked);
+        Assert.Equal(newSize, actualSize);
     }
 
     [Fact]
@@ -51,19 +53,21 @@ public class HeapTests
     }
 
     [Theory]
-    [InlineData(new[] { 0 }, -1, 0)]
-    [InlineData(new[] { 0 }, 1, 1)]
-    [InlineData(new[] { 0, 1 }, 2, 2)]
-    [InlineData(new[] { 0, 1 }, 1, 1)]
-    [InlineData(new[] { 0, 1, 2 }, 1, 2)]
-    public void Push_UpdatesRoot(int[] values, int newValue, int newRoot)
+    [InlineData(new[] { 0 }, -1, 0, 2)]
+    [InlineData(new[] { 0 }, 1, 1, 2)]
+    [InlineData(new[] { 0, 1 }, 2, 2, 3)]
+    [InlineData(new[] { 0, 1 }, 1, 1, 3)]
+    [InlineData(new[] { 0, 1, 2 }, 1, 2, 4)]
+    public void Push_UpdatesRoot(int[] values, int newValue, int newRoot, int newSize)
     {
         var maxHeap = new MaxHeap<int>(values);
 
         maxHeap.Push(newValue);
 
-        var actual = maxHeap.Peek();
+        var actualRoot = maxHeap.Peek();
+        var actualSize = maxHeap.Size;
 
-        Assert.Equal(newRoot, actual);
+        Assert.Equal(newRoot, actualRoot);
+        Assert.Equal(newSize, actualSize);
     }
 }
