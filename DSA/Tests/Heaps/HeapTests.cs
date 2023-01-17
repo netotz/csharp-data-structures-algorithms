7a4ac6354d13dd2a8d039266dffdc6b661e7c55f
netotz <neto.otz@hotmail.com>
2023-01-17 14:14:06 -0600

    tests(heap): add unit tests for Pop and Push

--- DSA/Tests/Heaps/HeapTests.cs
+++ DSA/Tests/Heaps/HeapTests.cs
@@ -9,12 +9,45 @@ public class HeapTests
     [InlineData(new[] { 0, 1 }, 1)]
     [InlineData(new[] { 0, 1, 2 }, 2)]
     [InlineData(new[] { 0, 3, 1, 2 }, 3)]
-    public void Peak_ReturnsHighestPriority(int[] values, int root)
+    public void Peak_ReturnsRoot(int[] values, int root)
     {
         var maxHeap = new MaxHeap<int>(values);
 
         var actual = maxHeap.Peak();
 
         Assert.Equal(root, actual);
+    }
+
+    [Theory]
+    [InlineData(new[] { 0, 1 }, 1, 0)]
+    [InlineData(new[] { 0, 1, 2 }, 2, 1)]
+    [InlineData(new[] { 0, 3, 1, 2 }, 3, 2)]
+    [InlineData(new[] { 0, 3, 1, 2, 4 }, 4, 3)]
+    public void Pop_ReturnsRootAndHeapifies(int[] values, int popped, int newRoot)
+    {
+        var maxHeap = new MaxHeap<int>(values);
+
+        var actualPopped = maxHeap.Pop();
+        var actualPeaked = maxHeap.Peak();
+
+        Assert.Equal(popped, actualPopped);
+        Assert.Equal(newRoot, actualPeaked);
+    }
+
+    [Theory]
+    [InlineData(new[] { 0 }, -1, 0)]
+    [InlineData(new[] { 0 }, 1, 1)]
+    [InlineData(new[] { 0, 1 }, 2, 2)]
+    [InlineData(new[] { 0, 1 }, 1, 1)]
+    [InlineData(new[] { 0, 1, 2 }, 1, 2)]
+    public void Push_UpdatesRoot(int[] values, int newValue, int newRoot)
+    {
+        var maxHeap = new MaxHeap<int>(values);
+
+        maxHeap.Push(newValue);
+
+        var actual = maxHeap.Peak();
+
+        Assert.Equal(newRoot, actual);
     }
 }
