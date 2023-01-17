ed5e2d1f0b686e296ddbf77eea2359045f37576b
netotz <neto.otz@hotmail.com>
2023-01-17 14:43:18 -0600

    feat(heaps): add parameterless constructor

--- DSA/Core/Heaps/MaxHeap.cs
+++ DSA/Core/Heaps/MaxHeap.cs
@@ -4,6 +4,8 @@ namespace DSA.Core.Heaps;
 
 public class MaxHeap<T> : Heap<T> where T : INumber<T>
 {
+    public MaxHeap() : base() { }
+
     public MaxHeap(ICollection<T> values) : base(values) { }
 
     protected override bool HasPriority(T value1, T value2)
