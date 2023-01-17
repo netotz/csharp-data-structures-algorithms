84e91ce3c45b71f2da7c8ea0d96b66b22b42ee61
netotz <neto.otz@hotmail.com>
2023-01-17 14:31:37 -0600

    feat(heaps): add MinHeap class

---
+++ DSA/Core/Heaps/MinHeap.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+
+namespace DSA.Core.Heaps;
+
+public class MinHeap<T> : Heap<T> where T : INumber<T>
+{
+    public MinHeap(ICollection<T> values) : base(values) { }
+
+    protected override bool HasPriority(T value1, T value2)
+    {
+        return value1 <= value2;
+    }
+}
